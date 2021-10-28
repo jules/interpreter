bca6af248b6bca561f66f12620794e0aebd1e492
Jules de Smit <julesdesmit@gmail.com>
2021-10-27 19:43:53 -0500

    Implement first objects

---
+++ src/object.rs
@@ -0,0 +1,15 @@
+pub enum Object {
+    Integer { value: i64 },
+    Boolean { value: bool },
+    Null,
+}
+
+impl Object {
+    pub fn inspect(&self) -> String {
+        match self {
+            Object::Integer { value } => format!("{}", value),
+            Object::Boolean { value } => format!("{}", value),
+            Object::Null => String::from("null"),
+        }
+    }
+}
